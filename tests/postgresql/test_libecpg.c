d01a276c3567fc51266383d23a267f9734afe4b9
Anton Novojilov <andy@essentialkaos.com>
2023-02-16 14:11:46 +0300

    Improve PostgreSQL tests

---
+++ tests/postgresql/test_libecpg.c
@@ -0,0 +1,7 @@
+#include <ecpg_config.h>
+#include <ecpglib.h>
+#include <ecpgtype.h>
+
+int main() {
+  return 0;
+}
---
+++ tests/postgresql/test_libpgtypes.c
@@ -0,0 +1,10 @@
+#include <pgtypes.h>
+#include <pgtypes_date.h>
+#include <pgtypes_error.h>
+#include <pgtypes_interval.h>
+#include <pgtypes_numeric.h>
+#include <pgtypes_timestamp.h>
+
+int main() {
+  return 0;
+}
