62f108b6b48c0e3ef00ffdd3fa4da79fd6742d8a
Anton Novojilov <andy@essentialkaos.com>
2024-01-17 23:15:28 +0300

    Remove luajit tests

--- tests/luajit/test.c
+++
@@ -1,9 +0,0 @@
-#include <luajit-2.0/lauxlib.h>
-#include <luajit-2.0/lua.h>
-#include <luajit-2.0/luaconf.h>
-#include <luajit-2.0/luajit.h>
-#include <luajit-2.0/lualib.h>
-
-int main() {
-  return 0;
-}
